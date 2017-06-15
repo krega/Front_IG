ea0da5b51f583b99b1bc70dfb0d2ab6f6ed74665
krega <kamil.regoc@gmail.com>
2017-06-16 01:53:23 +0200

    print specific stats

--- sites/sript2.js
+++ sites/sript2.js
@@ -1,57 +1,86 @@
 
 function start(a){
-function asyncForEach(items, cb) {
-    return items.reduce((p,c) => {
-        return p.then(value => cb(c));
-    }, Promise.resolve());
-}
+    function asyncForEach(items, cb) {
+        return items.reduce((p,c) => {
+            return p.then(value => cb(c));
+        }, Promise.resolve());
+    }
 
 
-var items =  [a];
-console.log(a);
+    var items =  [a];
+    console.log(a);
 
 
-function HamtaPokemon(z) {
-    console.log(z);
-    return Promise.resolve(z);
-}
+    function HamtaPokemon(z) {
+        console.log(z);
+        return Promise.resolve(z);
+    }
 
-function cb(z) {
-    return HamtaPokemon(z)
+    function cb(z) {
+       return HamtaPokemon(z)
         .then(data => {
             console.dir(data);
-           
-            var pokId = 'pokId' + z;
-            var pokName = 'pokName' + z;
-            var pokImage = 'pokImage' + z;
-            document.getElementById((pokId).toString()).innerHTML = data.id;
-            
-            var tab[]=data.stats.base_stat;
-            console.log(tab);
-          //  document.getElementById((pokName).toString()).innerHTML = data.stats.base_stat;
-            document.getElementById((pokImage).toString()).innerHTML = '<img src=http://img.pokemondb.net/artwork/' + data.name + '.jpg>';
-       
-        });
-}
 
-asyncForEach(items, cb)
+           // var pokId = 'pokId' + z;
+         //   var pokName = 'pokName' + z;
+           //var pokImage = 'pokImage' + z;
+           // document.getElementById((pokId).toString()).innerHTML = data.id;
+
+           var tab = [JSON.stringify(data.stats[0].base_stat)];
+           console.log(tab);
+           document.getElementById("pokSpeed").innerHTML = JSON.stringify(data.stats[0].base_stat)
+           document.getElementById("pokAttack").innerHTML = JSON.stringify(data.stats[4].base_stat)
+           document.getElementById("pokDefence").innerHTML = JSON.stringify(data.stats[3].base_stat)
+          
+       return getPokemon(z)
+            .then(data2 => {
+            console.dir(data2.chain.evolves_to[0].species.name);
+             document.getElementById("pokReference").innerHTML = data2.chain.evolves_to[0].species.name;
+
+       });//return getPokemon(z)
+           // .then(data2 => {
+           // console.dir(data2.chain.evolves_to[0].species.name);
+         //    document.getElementById("pokReference").innerHTML = data2.pokemon_species[0].name
+        
+             });
+    }
+
+    asyncForEach(items, cb)
     .then(result => console.log(result));
 
 
 
 
-function HamtaPokemon(pokeid)
-{
-	let adress="http://pokeapi.co/api/v2/"+"pokemon/"+pokeid+"/";
-	let input=$.get(adress,(data, status)=>{
-		console.dir(data);
-		if(status=="success"){
-			return data;
-		}
-		else
-			console.log(status);
+    function HamtaPokemon(pokeid)
+    {
+      let adress="http://pokeapi.co/api/v2/"+"pokemon/"+pokeid+"/";
+       let input=$.get(adress,(data, status)=>{
+          console.dir(data);
+          if(status=="success"){
+             return data;
+         }
+         else
+             console.log(status);
 
-	},"json");
-	return input;
-}
+     },"json");
+       return input;
+   }
+
+
+  
+    function getPokemon(pokeid)
+    {
+       let adress="http://pokeapi.co/api/v2/"+"evolution-chain/"+pokeid+"/";
+       let input=$.get(adress,(data, status)=>{
+          console.dir(data);
+          if(status=="success"){
+             return data;
+         }
+         else
+             console.log(status);
+
+     },"json");
+       return input;
+   }
+
 }
