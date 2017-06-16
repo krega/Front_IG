40cd3663537c3199c0af6e8c09c34b9bf4791edd
krega <kamil.regoc@gmail.com>
2017-06-16 09:26:28 +0200

    reference to evolution

--- sites/sript2.js
+++ sites/sript2.js
@@ -17,70 +17,41 @@ function start(a){
     }
 
     function cb(z) {
-       return HamtaPokemon(z)
-        .then(data => {
-            console.dir(data);
-
-           // var pokId = 'pokId' + z;
-         //   var pokName = 'pokName' + z;
-           //var pokImage = 'pokImage' + z;
-           // document.getElementById((pokId).toString()).innerHTML = data.id;
-
-           var tab = [JSON.stringify(data.stats[0].base_stat)];
-           console.log(tab);
-           document.getElementById("pokSpeed").innerHTML = JSON.stringify(data.stats[0].base_stat)
-           document.getElementById("pokAttack").innerHTML = JSON.stringify(data.stats[4].base_stat)
-           document.getElementById("pokDefence").innerHTML = JSON.stringify(data.stats[3].base_stat)
-          
-       return getPokemon(z)
-            .then(data2 => {
-            console.dir(data2.chain.evolves_to[0].species.name);
-             document.getElementById("pokReference").innerHTML = data2.chain.evolves_to[0].species.name;
-
-       });//return getPokemon(z)
-           // .then(data2 => {
-           // console.dir(data2.chain.evolves_to[0].species.name);
-         //    document.getElementById("pokReference").innerHTML = data2.pokemon_species[0].name
+     return HamtaPokemon(z)
+     .then(data => {
+        console.dir(data);
+        var tab = [JSON.stringify(data.stats[0].base_stat)];
+        console.log(tab);
+        document.getElementById("pokSpeed").innerHTML = JSON.stringify(data.stats[0].base_stat)
+        document.getElementById("pokAttack").innerHTML = JSON.stringify(data.stats[4].base_stat)
+        document.getElementById("pokDefence").innerHTML = JSON.stringify(data.stats[3].base_stat)
+       
         
-             });
-    }
+    });
+ }
 
-    asyncForEach(items, cb)
-    .then(result => console.log(result));
-
-
+ asyncForEach(items, cb)
+ .then(result => console.log(result));
 
 
-    function HamtaPokemon(pokeid)
-    {
-      let adress="http://pokeapi.co/api/v2/"+"pokemon/"+pokeid+"/";
-       let input=$.get(adress,(data, status)=>{
-          console.dir(data);
-          if(status=="success"){
-             return data;
-         }
-         else
-             console.log(status);
 
-     },"json");
-       return input;
+
+ function HamtaPokemon(pokeid)
+ {
+  let adress="http://pokeapi.co/api/v2/"+"pokemon/"+pokeid+"/";
+  let input=$.get(adress,(data, status)=>{
+      console.dir(data);
+      if(status=="success"){
+       return data;
    }
+   else
+       console.log(status);
+
+},"json");
+  return input;
+}
 
 
-  
-    function getPokemon(pokeid)
-    {
-       let adress="http://pokeapi.co/api/v2/"+"evolution-chain/"+pokeid+"/";
-       let input=$.get(adress,(data, status)=>{
-          console.dir(data);
-          if(status=="success"){
-             return data;
-         }
-         else
-             console.log(status);
 
-     },"json");
-       return input;
-   }
 
 }
